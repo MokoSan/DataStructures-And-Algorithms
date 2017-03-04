dd5e98dca6f821a94b08bfec7e97b6ee9d2c3b17
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-04 15:54:27 -0500

    LinkedLists: Added Question3 with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Data Structures/Linked Lists/CTCI/Question3.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists;
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.CTCI;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Data_Structures.Linked_Lists.CTCI
+{
+    [TestClass]
+    public class Question3
+    {
+        [TestMethod]
+        public void DeleteNodeInTheMiddle_EmptyList_NoOp()
+        {
+            ILinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.DeleteNodeInTheMiddle(new Node<int>(5));
+            Assert.IsTrue(linkedList.Length == 0);
+        }
+
+        [TestMethod]
+        public void DeleteNodeInTheMiddle_NullInput_NoOp()
+        {
+            ILinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddToHead(1);
+            Assert.IsTrue(linkedList.Length == 1);
+            linkedList.DeleteNodeInTheMiddle(null);
+            Assert.IsTrue(linkedList.Length == 1);
+        }
+
+        [TestMethod]
+        public void DeleteNodeInTheMiddle_NextNodeNull_SetNodeNull()
+        {
+            ILinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AppendToTail(1);
+            Assert.IsTrue(linkedList.Length == 1);
+        }
+
+        [TestMethod]
+        public void DeleteNodeInTheMiddle_NextNodeNotNull_RemoveCurrentNode()
+        {
+            ILinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AppendToTail(1);
+            linkedList.AppendToTail(2);
+            linkedList.AppendToTail(3);
+            Assert.IsTrue(linkedList.Length == 3);
+
+            INode<int> secondNode = linkedList.Head.Next;
+            linkedList.DeleteNodeInTheMiddle(secondNode);
+            Assert.IsTrue(linkedList.Length == 2);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Data Structures/Linked Lists/CTCI/Question3.cs
@@ -0,0 +1,30 @@
+﻿using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+
+namespace DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.CTCI
+{
+    /// <summary>
+    /// Implement an algorithm to delete a node in the 
+    /// middle of a single linked list, given only access to that node.
+    /// </summary>
+    public static class Question3
+    {
+        public static void DeleteNodeInTheMiddle<T>(this ILinkedList<T> linkedList,
+                                                    INode<T> nodeToDelete)
+        {
+            if ( nodeToDelete == null )
+            {
+                return;
+            }
+
+            if ( nodeToDelete.Next == null )
+            {
+                nodeToDelete = null;
+                return;
+            }
+
+            nodeToDelete.Data = nodeToDelete.Next.Data;
+            nodeToDelete.Next = nodeToDelete.Next.Next;
+            linkedList.RemovedElement();
+        }
+    }
+}
