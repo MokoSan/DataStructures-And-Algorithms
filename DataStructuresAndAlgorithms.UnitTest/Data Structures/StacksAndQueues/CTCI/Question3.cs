931d61f567a857a7860e9d781304564458d34042
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-04 22:47:04 -0500

    StacksAndQueues: Added the 3rd question with unit tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Data Structures/StacksAndQueues/CTCI/Question3.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Data_Structures.StacksAndQueues.CTCI
+{
+    [TestClass]
+    public class Question3
+    {
+        [TestMethod]
+        public void Pop_EmptyStacksPop_NullResult()
+        {
+            var q3 = new DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.CTCI.Question3<int>(0);
+            INode<int> node = q3.Pop();
+            Assert.IsNull(node);
+        }
+
+        [TestMethod]
+        public void Push_Push1IntoSingleStack_PeakIs1()
+        {
+            var q3 = new DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.CTCI.Question3<int>(300);
+            q3.Push(1);
+            Assert.IsTrue(q3.NumberOfStacks == 1);
+            Assert.IsTrue(q3.Peak.Data == 1);
+        }
+
+        [TestMethod]
+        public void Push_Push1IntoMultipleStacksWith1Threshold_2ndStackPeak1()
+        {
+            var q3 = new DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.CTCI.Question3<int>(1);
+            q3.Push(2);
+            q3.Push(1);
+            Assert.IsTrue(q3.NumberOfStacks == 2);
+            Assert.IsTrue(q3.Peak.Data == 1);
+        }
+
+        [TestMethod]
+        public void Pop_PopFromMultipleStackToRemoveOneStack_PoppedResultHasData1()
+        {
+            var q3 = new DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.CTCI.Question3<int>(1);
+            q3.Push(2);
+            q3.Push(1);
+            q3.Pop();
+            Assert.IsTrue(q3.NumberOfStacks == 1);
+            Assert.IsTrue(q3.Peak.Data == 2);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Data Structures/StacksAndQueues/CTCI/Question3.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+using DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.Interface;
+
+namespace DataStructuresAndAlgorithms.Data_Structures.StacksAndQueues.CTCI
+{
+    /// <summary>
+    /// Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore,
+    /// in real life, we would likely start a new stack when the previous stack exceeds
+    /// some threshold.Implement a data structure SetOfStacks that mimics this. SetOfStacks
+    /// should be composed of several stacks, and should create a new stack once
+    /// the previous one exceeds capacity.SetOfStacks.push() and SetOfStacks.pop() should
+    /// behave identically to a single stack(that is, pop() should return the same values as it
+    /// would if there were just a single stack).
+    /// </summary>
+    public class Question3<T> : IStack<T> 
+    {
+        private List<IStack<T>> _stacks = new List<IStack<T>>();
+        private int _stackNumber        = -1;
+        private readonly uint _stackThreshold; 
+
+        public Question3(uint stackThreshold)
+        {
+            _stackThreshold = stackThreshold;
+        }
+
+        public INode<T> Peak { get { return _stacks[_stackNumber].Peak; }}
+        public uint Length { get { return _stacks[_stackNumber].Length; }}
+        public int NumberOfStacks { get { return _stacks.Count; }} 
+
+        public INode<T> Pop()
+        {
+            if ( _stackNumber == -1 )
+            {
+                return null;
+            }
+
+            INode<T> head = _stacks[_stackNumber].Pop();
+            if ( _stacks[_stackNumber].Length == 0 )
+            {
+                _stacks.RemoveAt(_stackNumber);
+                --_stackNumber;
+            }
+
+            return head;
+        }
+
+        public INode<T> PopAt(int index)
+        {
+            if ( index < 0 || index >= _stacks.Count )
+            {
+                return null;
+            }
+
+            return _stacks[index].Pop();
+        }
+
+        public void Push(T data)
+        {
+            if ( _stackNumber == -1 )
+            {
+                _stackNumber = 0;
+                _stacks.Add(new Stack<T>());
+            }
+
+            if ( _stacks[_stackNumber].Length == _stackThreshold )
+            {
+                ++_stackNumber;
+                _stacks.Add(new Stack<T>());
+            }
+
+            _stacks[_stackNumber].Push(data);
+        }
+    }
+}
