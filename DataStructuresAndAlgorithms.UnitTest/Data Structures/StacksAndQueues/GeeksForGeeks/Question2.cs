2f3626ec33377ae47fd5bbcde4822d335c28ab01
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-10 18:23:29 -0500

    LinkedLists: StrCompare with LinkedLists with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Data Structures/StacksAndQueues/GeeksForGeeks/Question2.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists;
+using q2 = DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.GeeksForGeeks.Question2;
+using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Data_Structures.StacksAndQueues.GeeksForGeeks
+{
+    [TestClass]
+    public class Question2
+    {
+        [TestMethod]
+        public void StrCompareForLinkedList_BothListsEmpty_0()
+        {
+            int result = q2.StrCompareForLinkedLists(new LinkedList<char>(), new LinkedList<char>());
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void StrCompareForLinkedList_FirstListEmpty_Negative1()
+        {
+            ILinkedList<char> linkedList = new LinkedList<char>();
+            linkedList.AddToHead('c');
+            int result = q2.StrCompareForLinkedLists(new LinkedList<char>(), linkedList);
+            Assert.IsTrue(result == -1);
+        }
+
+        [TestMethod]
+        public void StrCompareForLinkedList_SecondLisEmpty_Positive1()
+        {
+            ILinkedList<char> linkedList = new LinkedList<char>();
+            linkedList.AddToHead('c');
+            int result = q2.StrCompareForLinkedLists(linkedList, new LinkedList<char>());
+            Assert.IsTrue(result == 1);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Data Structures/Linked Lists/GeeksForGeeks/Question2.cs
@@ -0,0 +1,51 @@
+﻿using DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.Interface;
+
+namespace DataStructuresAndAlgorithms.Data_Structures.Linked_Lists.GeeksForGeeks
+{
+    public static class Question2
+    {
+        public static int StrCompareForLinkedLists(ILinkedList<char> list1, ILinkedList<char> list2)
+        {
+            if ( list1.Head == null && list2.Head == null )
+            {
+                return 0;
+            }
+
+            if ( list1.Head != null && list2.Head == null )
+            {
+                return 1;
+            }
+
+            if ( list1.Head == null && list2.Head != null )
+            {
+                return -1; 
+            }
+
+            INode<char> runner1 = list1.Head;
+            INode<char> runner2 = list2.Head;
+
+            while ( runner1 != null && runner2 != null && runner1.Data == runner2.Data )
+            {
+                runner1 = runner1.Next;
+                runner2 = runner2.Next;
+            }
+
+            if ( runner1 != null && runner2 != null )
+            {
+                return (runner1.Data > runner2.Data) ? -1 : 1;
+            }
+
+            else if ( runner1 == null && runner2 != null )
+            {
+                return 1;
+            }
+
+            else if ( runner1 != null && runner2 == null )
+            {
+                return -1;
+            }
+
+            return 0;
+        } 
+    }
+}
