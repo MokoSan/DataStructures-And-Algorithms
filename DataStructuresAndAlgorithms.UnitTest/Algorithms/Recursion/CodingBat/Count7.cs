12818043af197378714c1b92d87f7f4dbea5e031
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 09:25:15 -0500

    Recursion: Added the Count7 method with unit tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/Count7.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.Count7;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class Count7 
+    {
+        [TestMethod]
+        public void Count7s_0Input_0Result()
+        {
+            int result = Count7s(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void Count7s_7Input_1Result()
+        {
+            int result = Count7s(7);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void Count7s_777Input_3Result()
+        {
+            int result = Count7s(777);
+            Assert.IsTrue(result == 3);
+        }
+
+        [TestMethod]
+        public void Count7s_727Input_2Result()
+        {
+            int result = Count7s(727);
+            Assert.IsTrue(result == 2);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/Count7.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class Count7
+    {
+        public static int Count7s(int digit)
+        {
+            return Count7sUtil(digit, 0);
+        }
+
+        public static int Count7sUtil(int digit, int runningCount)
+        {
+            if (digit == 0)
+                return runningCount;
+            bool isRightMostDigit7 = ( digit % 10 ) == 7;
+            runningCount += (isRightMostDigit7) ? 1 : 0;
+            return Count7sUtil(digit / 10, runningCount);
+        }
+    }
+}
