e2444351d8535dbff32fff9d2f67660ce23360f3
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 09:15:59 -0500

    Recursion: Added the SumOfDigits with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/SumDigits.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.SumDigits;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class SumDigits
+    {
+        [TestMethod]
+        public void ComputeSumDigits_0Input_0Result()
+        {
+            int result = ComputeSumDigits(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void ComputeSumDigits_1Input_1Result()
+        {
+            int result = ComputeSumDigits(1);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void ComputeSumDigits_123Input_6Result()
+        {
+            int result = ComputeSumDigits(123);
+            Assert.IsTrue(result == 6);
+        }
+
+        [TestMethod]
+        public void ComputeSumDigits_666Input_18Result()
+        {
+            int result = ComputeSumDigits(666);
+            Assert.IsTrue(result == 18);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/SumDigits.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class SumDigits
+    {
+        public static int ComputeSumDigits(int digit)
+        {
+            return ComputeSumDigitsUtil(digit, 0);
+        }
+
+        public static int ComputeSumDigitsUtil(int digitsLeft, int runningSum)
+        {
+            if (digitsLeft == 0)
+                return runningSum;
+            return ComputeSumDigitsUtil(digitsLeft / 10, runningSum + digitsLeft % 10);
+        }
+    }
+}
