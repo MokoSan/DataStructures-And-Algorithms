856bd6cf42624d30750d77a1cc4b6d2da2ae799f
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 08:39:59 -0500

    BunnyEars: Added the Bunny Ears Computation with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/BunnyEars.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.BunnyEars;
+    
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class BunnyEars
+    {
+        [TestMethod]
+        public void ComputeEars_0Bunnies_0Result()
+        {
+            int result = ComputeEars(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void ComputeEars_1Bunnies_1Result()
+        {
+            int result = ComputeEars(1);
+            Assert.IsTrue(result == 2);
+        }
+        
+        [TestMethod]
+        public void ComputeEars_2Bunnies_4Result()
+        {
+            int result = ComputeEars(2);
+            Assert.IsTrue(result == 4);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/BunnyEars.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    /// <summary>
+    /// We have a number of bunnies and each bunny has two big floppy ears.
+    /// We want to compute the total number of ears across all the bunnies recursively.
+    /// </summary>
+    public static class BunnyEars
+    {
+        public static int ComputeEars(int numberOfBunnies)
+        {
+            if (numberOfBunnies == 0)
+                return 0;
+
+            return 2 + ComputeEars(numberOfBunnies - 1);
+        }
+    }
+}
