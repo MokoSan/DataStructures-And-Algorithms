da5bf8fbf8b82710c8a918e12cf39865e679c809
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 08:22:02 -0500

    Recursion: Added the Factorial Computation with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/Factorial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion
+{
+    [TestClass]
+    public class Factorial
+    {
+        [TestMethod]
+        public void ComputeFactorial_0Input_1Output()
+        {
+            int result = DataStructuresAndAlgorithms.Algorithms.Recursion.Factorial.ComputeFactorial(0);
+            Assert.IsTrue(result == 1);
+        } 
+
+        [TestMethod]
+        public void ComputeFactorial_1Input_1Output()
+        {
+            int result = DataStructuresAndAlgorithms.Algorithms.Recursion.Factorial.ComputeFactorial(1);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void ComputeFactorial_5Input_120Output()
+        {
+            int result = DataStructuresAndAlgorithms.Algorithms.Recursion.Factorial.ComputeFactorial(5);
+            Assert.IsTrue(result == 120);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/Factorial.cs
@@ -0,0 +1,15 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion
+{
+    public static class Factorial
+    {
+        public static int ComputeFactorial(int n)
+        {
+            if ( n == 0 || n == 1 )
+            {
+                return 1;
+            }
+
+            return n * ComputeFactorial(n - 1);
+        }
+    }
+}
