c3d3b3cedb43a2f1c7a9cedd66ba4082f8ecbbaf
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 08:56:29 -0500

    Recursion: BunnyEars2 added with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/BunnyEars2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.BunnyEars2;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class BunnyEars2 
+    {
+        [TestMethod]
+        public void ComputeBunnyEars2_0Input_0Result()
+        {
+            int result = ComputeBunnyEars2(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void ComputeBunnyEars2_1Input_2Result()
+        {
+            int result = ComputeBunnyEars2(1);
+            Assert.IsTrue(result == 2);
+        }
+
+        [TestMethod]
+        public void ComputeBunnyEars2_2Input_5Result()
+        {
+            int result = ComputeBunnyEars2(2);
+            Assert.IsTrue(result == 5);
+        }
+
+        [TestMethod]
+        public void ComputeBunnyEars2_3Input_7Result()
+        {
+            int result = ComputeBunnyEars2(3);
+            Assert.IsTrue(result == 7);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/BunnyEars2.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    /// <summary>
+    /// We have bunnies standing in a line, numbered 1, 2, ... 
+    /// The odd bunnies (1, 3, ..) have the normal 2 ears. 
+    /// The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. 
+    /// Recursively return the number of "ears" in the bunny line 1, 2, ... n 
+    /// </summary>
+    public static class BunnyEars2
+    {
+        public static int ComputeBunnyEars2(int numberOfBunnies)
+        {
+            return ComputeBunnyEars2Util(numberOfBunnies, 0);
+        }
+
+        public static int ComputeBunnyEars2Util(int numberOfBunnies, int runningSum)
+        {
+            if (numberOfBunnies == 0)
+                return runningSum;
+            runningSum += (numberOfBunnies % 2 == 0) ? 3 : 2;
+            return ComputeBunnyEars2Util(numberOfBunnies - 1, runningSum);
+        }
+    }
+}
