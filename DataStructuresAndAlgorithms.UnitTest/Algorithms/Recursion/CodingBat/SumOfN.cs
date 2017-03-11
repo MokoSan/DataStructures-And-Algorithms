dcd5a93c61a7958a1d15c802607f44a84e2868a4
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 09:12:09 -0500

    Recursion: Added SumOfN with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/SumOfN.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.SumOfN;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class SumOfN
+    {
+        [TestMethod]
+        public void ComputeSumOfN_0Input_0Result()
+        {
+            int result = ComputeSumTillN(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void ComputeSumOfN_1Input_1Result()
+        {
+            int result = ComputeSumTillN(1);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void ComputeSumOfN_2Input_3Result()
+        {
+            int result = ComputeSumTillN(2);
+            Assert.IsTrue(result == 3);
+        }
+
+        [TestMethod]
+        public void ComputeSumOfN_3Input_6Result()
+        {
+            int result = ComputeSumTillN(3);
+            Assert.IsTrue(result == 6);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/SumOfN.cs
@@ -0,0 +1,17 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class SumOfN
+    {
+        public static int ComputeSumTillN(int n)
+        {
+            return ComputeSumTillNUtil(n, 0);
+        }
+
+        public static int ComputeSumTillNUtil(int n, int runningSum)
+        {
+            if (n == 0)
+                return runningSum;
+            return ComputeSumTillNUtil(n - 1, runningSum + n);
+        }
+    }
+}
