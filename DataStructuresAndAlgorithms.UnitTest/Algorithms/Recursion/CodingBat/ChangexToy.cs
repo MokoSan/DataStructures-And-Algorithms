b6c5ea594e2efefaf5c3fc3b7349a6e2d4acb0bc
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 10:39:15 -0500

    Recursion: Added Changextoy with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/ChangexToy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.Changextoy;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class ChangexToy
+    {
+        [TestMethod]
+        public void ChangexToys_EmptyInput_0Result()
+        {
+            string result = ChangeAllxToy(string.Empty);
+            Assert.IsTrue(result == string.Empty);
+        }
+
+        [TestMethod]
+        public void ChangeAllxToy_xxInput_yyResult()
+        {
+            string result = ChangeAllxToy("xx");
+            Assert.IsTrue(result == "yy");
+        }
+
+        [TestMethod]
+        public void ChangeAllxToy_xyxyInput_yyyyResult()
+        {
+            string result = ChangeAllxToy("xyxy");
+            Assert.IsTrue(result == "yyyy");
+        }
+
+        [TestMethod]
+        public void ChangeAllxToy_yyyxInput_yyyyResult()
+        {
+            string result = ChangeAllxToy("yyyx");
+            Assert.IsTrue(result == "yyyy");
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/Changextoy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class Changextoy
+    {
+        public static string ChangeAllxToy(string input)
+        {
+            if (input.Length == 0)
+                return String.Empty;
+
+            StringBuilder output = new StringBuilder();
+            ChangeAllxToyUtil(input, output, 0);
+            return output.ToString();
+        }
+
+        public static void ChangeAllxToyUtil(string input, StringBuilder output, int currentPosition)
+        {
+            if (currentPosition == input.Length)
+                return;
+
+            if (input[currentPosition] == 'x')
+                output.Append('y');
+            else
+                output.Append(input[currentPosition]);
+
+            ChangeAllxToyUtil(input, output, currentPosition + 1);
+        }
+    }
+}
