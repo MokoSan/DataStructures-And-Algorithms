f776c93adbf8451509e2beaabda1713b23af6bbc
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 08:46:35 -0500

    Recursion: Added Fibonacci Computation with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/Fibonacci.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.Fibonacci;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class Fibonacci 
+    {
+        [TestMethod]
+        public void ComputeFibonacci_0Input_0Result()
+        {
+            int result = ComputeFibonnaci(0);
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void ComputeFibonacci_1Input_1Result()
+        {
+            int result = ComputeFibonnaci(1);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void ComputeFibonacci_2Input_1Result()
+        {
+            int result = ComputeFibonnaci(2);
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void ComputeFibonacci_3Input_2Result()
+        {
+            int result = ComputeFibonnaci(3);
+            Assert.IsTrue(result == 2);
+        }
+
+        [TestMethod]
+        public void ComputeFibonacci_4Input_3Result()
+        {
+            int result = ComputeFibonnaci(4);
+            Assert.IsTrue(result == 3);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class Fibonacci
+    {
+        public static int ComputeFibonnaci(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            if (n == 1)
+                return 1;
+
+            return ComputeFibonnaci(n - 1) + ComputeFibonnaci(n - 2);
+        }
+    }
+}
