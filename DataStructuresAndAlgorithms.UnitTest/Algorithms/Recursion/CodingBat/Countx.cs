0c7e40cb480beda296595c73f7ed3f12e8738fec
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2017-03-11 09:32:42 -0500

    Recursion: Added Countx with Unit Tests

---
+++ DataStructuresAndAlgorithms.UnitTest/Algorithms/Recursion/CodingBat/Countx.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat.Countx;
+
+namespace DataStructuresAndAlgorithms.UnitTest.Algorithms.Recursion.CodingBat
+{
+    [TestClass]
+    public class Countx 
+    {
+        [TestMethod]
+        public void CountAllxs_EmptyInput_0Result()
+        {
+            int result = CountAllxs("");
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void CountAllxs_xxxInput_3Result()
+        {
+            int result = CountAllxs("xxx");
+            Assert.IsTrue(result == 3);
+        }
+
+        [TestMethod]
+        public void CountAllxs_xxxyxxxInput_6Result()
+        {
+            int result = CountAllxs("xxxyxxx");
+            Assert.IsTrue(result == 6);
+        }
+
+        [TestMethod]
+        public void CountAllxs_yyyInput_0Result()
+        {
+            int result = CountAllxs("yyy");
+            Assert.IsTrue(result == 0);
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Recursion/CodingBat/Countx.cs
@@ -0,0 +1,19 @@
+﻿namespace DataStructuresAndAlgorithms.Algorithms.Recursion.CodingBat
+{
+    public static class Countx
+    {
+        public static int CountAllxs(string input)
+        {
+            return CountxUtil(input, 0, 0);
+        }
+
+        public static int CountxUtil(string input, int runningCount, int currentPosition)
+        {
+            if ( currentPosition == input.Length)
+                return runningCount;
+            if (input[currentPosition] == 'x')
+                runningCount += 1;
+            return CountxUtil(input, runningCount, currentPosition + 1);
+        }
+    }
+}
